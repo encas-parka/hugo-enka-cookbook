75183b544caaabe31985979d75a96dc7c8fd6966
Qaldek <qaldek@gmx.com>
2023-12-22 03:03:48 +0100

    conf+ import debug

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,53 +1,41 @@
 const autoprefixer = require('autoprefixer');
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./hugo_stats.json"],
-  defaultExtractor: (content) => {
-    const els = JSON.parse(content).htmlElements;
-    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
-  },
-  safelist: [],
-});
-
+const purgecss = require('@fullhuman/postcss-purgecss');
 const whitelister = require('purgecss-whitelister');
-// const cssbyebye = require('css-byebye');
 
 module.exports = {
   plugins: [
     autoprefixer(),
     purgecss({
-      content: [
-        './node_modules/@hyas/core/layouts/**/*.html',
-        './layouts/**/*.html',
-        './content/**/*.md',
+      content: [ './hugo_stats.json' ],
+      extractors: [
+        {
+          extractor: (content) => {
+            const els = JSON.parse(content).htmlElements;
+            return els.tags.concat(els.classes, els.ids);
+          },
+          extensions: ['json'],
+        },
+      ],
+      dynamicAttributes: [
+        'aria-expanded',
+        'id',
+        'size',
+        'type',
       ],
       safelist: [
+        'active',
+        'disabled',
+        'hidden',
+        'show',
+        'img-fluid',
+        'blur-up',
         'lazyloaded',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        'h3',
-        'alert-link',
         ...whitelister([
-          
-          //'./assets/scss/components/_alerts.scss', //// enlevé avec v.2.03
-          // './assets/scss/components/_buttons.scss',
-          //'./assets/scss/components/_code.scss', //// enlevé avec v.2.03
-          // './assets/scss/components/_syntax.scss',
-          // './node_modules/@hyas/core/assets/scss/app.scss',
-          // './node_modules/vuetify/dist/vuetify.min.css',
+          './assets/scss/**/*.css',
+          './assets/scss/**/*.scss',
+          './node_modules/katex/dist/katex.css',
         ]),
       ],
     }),
-    // cssbyebye({
-    //   rulesToRemove: [
-    //     /.*\.v-application--is-rtl.*/,
-    //     /.*\.theme--dark.*/,
-        
-    //   ]
-    // }),
-
   ],
 }
